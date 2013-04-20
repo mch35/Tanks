2ad4077832853485b03b677ff575eb0265d5195e
Michal Chilczuk <michalchilczuk@gmail.com>
2013-04-20 18:22:49 +0200

    First implementation

---
+++ Tanks/Tanks.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Micha³ Chilczuk
+ *
+ */
+public class Tanks {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
